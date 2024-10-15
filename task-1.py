0db4128a03b2f0c54106cbe0f02a8e1878f98a11
antonysavache <89783847+antonysavache@users.noreply.github.com>
2024-10-15 11:39:22 +0300

    123

---
+++ task-1.py
@@ -0,0 +1,32 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n: int):
+        try:
+            fibo = [0]
+
+            if n in cache.keys(): return cache[n]
+            elif n == 0: return fibo
+            else: fibo.append(1)
+
+
+            while len(fibo) < n:
+                fibo.append(fibo[-1] + fibo[-2])
+            cache[n] = fibo
+            print(cache)
+            return cache[n]
+        except Exception as e:
+            return e
+    return fibonacci
+
+fib = caching_fibonacci()
+
+print(fib(-3))
+print(fib(11))
+print(fib(12))
+print(fib(10))
+
+
+
+
+
